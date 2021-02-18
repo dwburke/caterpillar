6c0f73a8142a3e68ff44f4b3f5d31e724500660b
Dan Burke <dburke@addictmud.org>
2021-02-18 18:06:30 -0500

    wip

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dwburke/copyman/syncdir"
+)
 
 func main() {
 	fmt.Println("vim-go")
---
+++ syncdir/syncdir.go
@@ -0,0 +1,8 @@
+package syncdir
+
+import (
+//"github.com/otiai10/copy"
+)
+
+func Sync() {
+}
