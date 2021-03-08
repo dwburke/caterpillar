2de5fbc22cbb46ac74ec98fc5c12fa1ae4f0ba25
Dan Burke <dburke@addictmud.org>
2021-03-07 23:09:19 -0500

    Closer to what I need for pathing

--- cmd/hash.go
+++ cmd/hash.go
@@ -2,12 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/karrick/godirwalk"
 	"github.com/spf13/cobra"
 
 	"github.com/dwburke/caterpillar/hash"
+	"github.com/dwburke/caterpillar/util"
 )
 
 func init() {
@@ -21,8 +24,10 @@ var hashCmd = &cobra.Command{
 	Short:   "hash",
 	Long:    `hash`,
 	Args:    cobra.ExactArgs(1),
-	Example: "hash foo/",
+	Example: "hash foo",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		var files []*File
 
 		//if str, err := hash.Md5File("/etc/hosts"); err != nil {
 		//panic(err)
@@ -30,8 +35,14 @@ var hashCmd = &cobra.Command{
 		//fmt.Println(str)
 		//}
 
+		dir := util.TrimSuffix(args[0], "/")
+		dir, err := filepath.Abs(dir)
+		if err != nil {
+			panic(err)
+		}
+
 		//dirname := "some/directory/root"
-		err := godirwalk.Walk(args[0], &godirwalk.Options{
+		err = godirwalk.Walk(dir, &godirwalk.Options{
 			Callback: func(osPathname string, de *godirwalk.Dirent) error {
 				// Following string operation is not most performant way
 				// of doing this, but common enough to warrant a simple
@@ -44,6 +55,20 @@ var hashCmd = &cobra.Command{
 					return nil
 				}
 
+				cwd, err := os.Getwd()
+				if err != nil {
+					return err
+				}
+
+				rel, err := filepath.Rel(cwd, osPathname)
+				if err != nil {
+					return err
+				}
+
+				//dir := filepath.Dir(osPathname)
+				parent := filepath.Base(dir)
+				fmt.Printf("%s\n%s/%s\n", osPathname, parent, rel)
+
 				str, err := hash.Md5File(osPathname)
 				if err != nil {
 					return err
@@ -54,6 +79,9 @@ var hashCmd = &cobra.Command{
 					str,
 					osPathname)
 
+				f := &File{Name: fmt.Sprintf("%s/%s", parent, rel), Hash: str}
+				files = append(files, f)
+
 				return nil
 			},
 			Unsorted: false, // (optional) set true for faster yet non-deterministic enumeration (see godoc)
@@ -62,5 +90,17 @@ var hashCmd = &cobra.Command{
 		if err != nil {
 			panic(err)
 		}
+
+		err = util.JsonWrite(dir+".json", files)
+		if err != nil {
+			panic(err)
+		}
+
+		//if b, err := json.MarshalIndent(files, "", "  "); err != nil {
+		//panic(err)
+		//} else {
+		//fmt.Println(string(b))
+		//}
+
 	},
 }
